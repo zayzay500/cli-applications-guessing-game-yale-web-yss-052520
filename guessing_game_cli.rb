c2e73112000a3eea116810de65854129a181fb83
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-25 01:07:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,10 @@ def run_guessing_game
   puts "Please make a guess between 1 and 6"
   input = gets.chomp
   case input
-  
+  when num
+    puts "You guessed the correct number!"
+  when != num
+    puts "Sorry! The computer guessed #{num}"
+  when "exit"
+    puts "Goodbye!"
 end
